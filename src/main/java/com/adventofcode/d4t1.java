d90ec169afebcfd2e6cb237734b0c14ef6637128
nrozyc <nrozyc@amelco.co.uk>
2019-12-12 14:43:12 +0000

    more work on d4t2

--- src/main/java/com/adventofcode/d4t1.java
+++ src/main/java/com/adventofcode/d4t1.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class d4t1 {
 
+    //366 too low
+
     public static boolean isValid (int num) {
 
         int temp = num;
@@ -32,11 +34,37 @@ public class d4t1 {
         return isIncreasing && isPair;
 
     }
+
+    public static boolean ifTriple (int psswrd) {
+
+        int temp = psswrd;
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (temp > 0) {
+            digits.add(temp % 10);
+            temp /= 10;
+        }
+
+        Collections.reverse(digits);
+
+       boolean ifTriple = false;
+
+        for (int i = 2; i < digits.size(); i++) {
+            if (digits.get(i).equals(digits.get(i - 1)) && digits.get(i).equals(digits.get(i - 2))) {
+                ifTriple = true;
+                break;
+            }
+        }
+
+        return ifTriple;
+
+    }
+
     public static void main(String[] args) {
         int start = 264793;
         int finish = 803935;
 
         List<Integer> correctPasswords = new ArrayList<>();
+        List<Integer> correctPasswordsWithoutTriples = new ArrayList<>();
 
         for (int i = start; i <= finish ; i++) {
             if (isValid(i)) {
@@ -45,7 +73,16 @@ public class d4t1 {
 
         }
 
-        System.out.println(correctPasswords.size());
+        for (Integer psswrd: correctPasswords) {
+            if (!ifTriple(psswrd)) {
+                correctPasswordsWithoutTriples.add(psswrd);
+            }
+        }
+
+        //System.out.println(correctPasswords.size());
+        //System.out.println(correctPasswordsWithoutTriples.size());
+
+        System.out.println();
     }
 
 }
